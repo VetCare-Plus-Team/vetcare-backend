72bf30bb1f0fae0f31ce3c31285aadf11bbeada4
PANCHAYUBHANU <89331807+PANCHAYU-BHANU@users.noreply.github.com>
2026-03-05 13:05:20 +0530

    i create entity file
    create entity file

---
+++ pet-profile-service/src/main/java/com/vetcare/pet_profile_service/entity/Pet.java
@@ -0,0 +1,33 @@
+package com.vetcare.pet_profile_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String species; // e.g., Dog, Cat
+    private String breed;
+    private Integer age;
+    private String gender;
+    
+    // Relational mapping Owner
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+    
+    private String qrCodeFileName;
+}
